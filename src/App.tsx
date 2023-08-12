7f3df9f6ca493a085c69655d9ddf763777654838
Swapneel Datta <swd543@gmail.com>
2023-08-12 22:30:43 +0200

    Beautify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, createEffect, createSignal } from 'solid-js';
+import { Component, createEffect, createSignal, onCleanup, onMount } from 'solid-js';
 
 import styles from './App.module.css';
 import { Board, Coord, GolNaive } from './Board';
@@ -16,7 +16,7 @@ const BoardApp = (props: { b: Board<boolean> }) => (
 
 const App: Component = () => {
 
-  const initBoard = () => new GolNaive(200, 100, () => Math.random() > 0.90,
+  const initBoard = (l = 200, b = 100) => new GolNaive(l, b, () => Math.random() > 0.90,
     [Coord.E, Coord.W, Coord.N, Coord.S, Coord.NE, Coord.NW, Coord.SE, Coord.SW], true)
 
   const [b, setB] = createSignal<Board<boolean>>(initBoard(), { equals: false });
@@ -27,17 +27,32 @@ const App: Component = () => {
     setB(b())
   }
 
+  const handler = (_event: Event) => {
+    const div = 10
+    const dims = [~~(window.innerWidth / div), ~~(window.innerHeight / div)]
+    setB(initBoard(...dims))
+  };
+
+  onMount(() => {
+    window.addEventListener('resize', handler);
+  });
+
+  onCleanup(() => {
+    window.removeEventListener('resize', handler);
+  })
+
   return (
     <div class={styles.App}>
       <h1>Welcome to a very poorly coded game of life</h1>
       <p>In order to make your own implementation of game of life, extend <strong>Board.ts/Board</strong> and then include it in the initBoard function in this file</p>
       <p>To test your implementation of game of life, change <strong>App.test.ts</strong> and run <strong>pnpm test</strong></p>
       <div>
-        <button onclick={react}>step</button>
+        <button class={styles.skewbutton} onclick={react}>Step</button>
+        <button class={styles.skewbutton} onclick={handler}>Reinitialize</button>
         {
           timer() ?
-            <button onClick={() => { clearInterval(timer()); setTimer(undefined) }}>pause</button> :
-            <button onclick={() => { setTimer(setInterval(() => react(), 100)) }}>resume</button>
+            <button class={styles.skewbutton} onClick={() => { clearInterval(timer()); setTimer(undefined) }}>Pause</button> :
+            <button class={styles.skewbutton} onclick={() => { setTimer(setInterval(() => react(), 100)) }}>Resume</button>
         }
       </div>
       <BoardApp b={b()} />
